f7d32fcb669049f606397987528739e90e5ac5f3
Rasmus <rasmus.h.e@hotmail.se>
2020-02-03 16:27:54 +0100

    make it better

--- commands/moderation/welcome.js
+++ commands/moderation/welcome.js
@@ -2,9 +2,10 @@ module.exports = {
     name: "weclome",
     category: "moderation",
     description: "welcomes users",
-    run: async (client, guildMemberAdd, guildMemberRemove, message, args, member) => {
+    run: async (client, message, args) => {
+        client.on("guildMemberAdd", (member) => {
             const guild = member.guild;
-            if (newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+            if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
             newUsers[guild.id].set(member.id, member.user);
           
             if (newUsers[guild.id].size > 10) {
@@ -12,7 +13,12 @@ module.exports = {
               guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
               newUsers[guild.id].clear();
             }
-            
+          });
+          
+          client.on("guildMemberRemove", (member) => {
+            const guild = member.guild;
             if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
           }
+        );
+    }
 }
