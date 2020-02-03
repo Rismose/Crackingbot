734f9ee642efd16b59b5464beb89769ebbc3f6bc
Rasmus <rasmus.h.e@hotmail.se>
2020-02-03 16:24:58 +0100

    make it better

--- commands/moderation/welcome.js
+++ commands/moderation/welcome.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "welcomes users",
     run: async (client, guildMemberAdd, guildMemberRemove, message, args, member) => {
             const guild = member.guild;
-            if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+            if (newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
             newUsers[guild.id].set(member.id, member.user);
           
             if (newUsers[guild.id].size > 10) {
