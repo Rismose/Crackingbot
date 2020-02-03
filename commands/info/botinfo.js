73b9902ca25f357fbef3640ce7ca443cb7ab0ca6
Rasmus <rasmus.h.e@hotmail.se>
2020-02-03 16:16:40 +0100

    make it better

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -13,7 +13,7 @@ module.exports = {
             description: "***About the bot***",
             fields: [{
                 name: "**Version**",
-                value: "1.0.7 Pre-Release."
+                value: "1.0.9 Pre-Release."
             },
             {
                 name: "**Creator**",
