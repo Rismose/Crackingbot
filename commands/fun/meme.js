1aa53d811b0c69ac854d7f8c72aa97d9afb99048
Rismose <47760012+Rismose@users.noreply.github.com>
2021-02-11 18:42:18 +0100

    fixing max's bs mistake

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const randomPuppy = require("random-puppy");
+const got = require('got');
 
 module.exports = {
     name: "meme",
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args) => {
         // In this array, 
         // you can put the subreddits you want to grab memes from
-        const subReddits = ["dankmeme", "meme", "me_irl", "memes", "boomermeme", "memeeconomy"];
+        const subReddits = ["dankmeme", "meme", "me_irl", "memes", "boomermeme", "memeeconomy", "BikiniBottomTwitter", "Memes_Of_The_Dank"];
         
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     
