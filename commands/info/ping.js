6f27eed88b07d0bb0a3a7e47a5507df5a73577ad
Rismose <47760012+Rismose@users.noreply.github.com>
2021-02-12 21:57:19 +0100

    "fixing" !ping

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -6,7 +6,6 @@ module.exports = {
         const msg = await message.channel.send(`🏓 Pinging....`);
 
         msg.edit(`🏓 Pong!
-        Latency is ${Math.floor(msg.createdAt - message.createdAt)}ms
-        API Latency is ${Math.round(client.ping)}ms`);
+        Latency is ${Math.floor(msg.createdAt - message.createdAt)}ms`);
     }
 }
